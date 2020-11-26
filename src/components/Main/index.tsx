57528d261fdf0c32e8bfdd842ea7f2720a3b363a
kukajp3 <kukajp@gmail.com>
2020-11-25 21:59:15 -0300

    initial boilerplate application

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/assets/logo.svg" alt="React avançado" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration src="/assets/hero-illustration.svg" alt="Desenvolvedor" />
+  </S.Wrapper>
+)
+
+export default Main
